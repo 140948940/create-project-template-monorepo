e0b438b35c40ae5d37ea2cd4ed4c6e33c35c209d
A SOUL <59335580+140948940@users.noreply.github.com>
2023-03-21 11:39:43 +0800

    chore: banner修改

--- test.mjs
+++
@@ -1,7 +0,0 @@
-import {replaceTextInDirAndFiles} from './utils/replaceTextInDirAndFiles.mjs'
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-replaceTextInDirAndFiles(path.join(__dirname,'templates'),'add-page-watermark','${template---name}')
-console.log(path.join(__dirname,'templates'))
